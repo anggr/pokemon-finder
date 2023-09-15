0d5d2203d357a9c105d378c1277fbf15387ffe13
anggr <anggorookky@gmail.com>
2023-09-15 23:36:15 +0800

    style(PokemonDetails): Simplify component structure and add styling: - display image side by side

--- src/components/PokemonDetails.tsx
+++ src/components/PokemonDetails.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 
-interface Props {
-  id?: number;
-  name?: string;
-}
+type PokemonDetailsProps = {
+  id: number;
+  name: string;
+};
 
-const PokemonDetails: React.FC<Props> = ({ id, name }) => {
-  if (!id || !name) {
-    return null;
-  }
-
+const PokemonDetails: React.FC<PokemonDetailsProps> = ({ id, name }) => {
   return (
-    <div className="mt-4">
-      <h2 className="text-2xl mb-2">{name}</h2>
+    <div className="h-44 w-32 bg-white rounded-xl flex flex-col items-center justify-center shadow duration-300 hover:bg-white hover:shadow-xl m-2">
       <img
         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
         alt={name}
+        className="self-center"
       />
+      <span className="mt-1 text-sm leading-5 font-semibold text-center">
+        {name}
+      </span>
     </div>
   );
 };
