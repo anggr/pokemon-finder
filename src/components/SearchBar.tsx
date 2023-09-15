77175f525a820e63cd1abb0263d70b12b6799bfa
anggr <anggorookky@gmail.com>
2023-09-15 23:05:49 +0800

    feat: add SearchBar component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+interface Props {
+  onSearch: (name: string) => void;
+}
+
+const SearchBar: React.FC<Props> = ({ onSearch }) => {
+  const [inputValue, setInputValue] = React.useState('');
+
+  const handleSearch = () => {
+    onSearch(inputValue);
+  };
+
+  return (
+    <div className="flex items-center">
+      <input
+        className="p-2 border rounded"
+        type="text"
+        placeholder="Search Pokemon..."
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button className="ml-2 p-2 bg-blue-500 text-white rounded" onClick={handleSearch}>
+        Search
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
